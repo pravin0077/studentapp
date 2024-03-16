f10406e3083580d78ec5345d321b7f86e79dc1e1
pravin0077 <161559209+pravin0077@users.noreply.github.com>
2024-03-17 00:14:27 +0530

    Add files via upload

---
+++ script.groovy
@@ -0,0 +1,22 @@
+/opt/apache-maven-3.6.3
+pipeline {
+    agent any
+
+    stages {
+
+        stage('Pull') {
+            steps {
+                git 'https://github.com/chetansomkuwar254/studentapp.ui.git'
+                echo 'Hello World'
+            }
+        }
+
+        stage('Build') {
+            steps {
+                sh '/opt/apache-maven-3.6.3/bin/mvn clean package'
+                echo 'Hello World'
+            }
+        }
+
+    }
+}
