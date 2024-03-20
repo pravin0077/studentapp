c2bd51073b25e28cc634b49582d61c5b0c117325
pravin0077 <161559209+pravin0077@users.noreply.github.com>
2024-03-21 02:53:11 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -18,7 +18,7 @@ pipeline {
         }
 stage('testing') {
             steps {
-                sh '/opt/apache-maven-3.6.3/bin/mvn sonar:sonar -Dsonar.projectKey=test -Dsonar.host.url=http://65.2.143.231:9000 -Dsonar.login=430c30e705b43f13c089e317b784786a4da07c38'
+                sh '/opt/apache-maven-3.6.3/bin/mvn sonar:sonar -Dsonar.projectKey=test -Dsonar.host.url=http://43.205.237.12:9000 -Dsonar.login=430c30e705b43f13c089e317b784786a4da07c38'
                 echo 'Testing done'
             }
         }
